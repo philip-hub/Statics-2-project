0078292715627225ae6f81879cf96e84273f74fe
philip-hub <philipstudentemail@gmail.com>
2024-10-01 18:29:28 -0400

    new commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#written by Philip Pounds
+
 import pandas as pd
 
 A_L = []
@@ -19,16 +21,14 @@ D_dx = 0.0625 # delta inches
 D_L=  0.0625 #inches
 D_S_O =  0.0625 #inches
 D_S_I =  0 #inches
-D_theta = 1.5*(3.14159265359/180) #degreea
+D_theta = 1.5*(3.14159265359/180) #rad
 T = 14180.70543 #lbf-in
 L_S = 26 #in
 L_L = 38 #in
 
 
-#long shaft
-
 while(D_L<7):
-    D_S_O =  0.0625 #inches 
+    D_S_O =  0.125 #inches 
     D_L_shear_pass = False
     D_L=D_L+D_dx
     c =(D_L/2)
@@ -55,7 +55,7 @@ while(D_L<7):
 
             theta = ((T*L_S)/(j_s*G))-((T*L_L)/(j*G)) #short - long
             
-            if((theta**2)<(D_theta**2)):
+            if abs((theta))<abs((D_theta)):
                 D_S_angle_pass = True
             
             
@@ -64,11 +64,11 @@ while(D_L<7):
                 D_long_list.append(D_L)
                 D_short_o_list.append(D_S_O)
                 D_short_i_list.append(D_S_I)
-                d_theta.append(theta)       
+                d_theta.append(theta*(180/Pi))       
                         
                         
-                Area_long = (Pi/2)*((D_L/2)**2)
-                Area_short = (Pi/2)*(((D_S_O/2)**2)-(D_S_I/2)**2)
+                Area_long = (Pi/2)*((D_L/2)**2)*38
+                Area_short = ((Pi/2)*(((D_S_O/2)**2)-(D_S_I/2)**2))*26
                 Area_total = Area_short+Area_long
                 
                 A_L.append(Area_long)
@@ -88,14 +88,14 @@ df = pd.DataFrame({
     "Long Shaft Diameter": D_long_list,
     "Short Shaft Outer Diameter": D_short_o_list,
     "Short Shaft Inner Diameter": D_short_i_list,
-    "Long Shaft Cross Sectional Area": A_L,
-    "Short Shaft Cross Sectional Area": A_S,
-    "Both Shafts total area": A_T,
+    "Long Shaft volume": A_L,
+    "Short Shaft volume": A_S,
+    "Both Shafts total volume": A_T,
     "Long Shaft Shear Stress": long_shear,
     "Short Shaft Shear Stress": short_shear,
     "Theta": d_theta
 })
 
 
-df_sorted = df.sort_values(by="Both Shafts total area")
+df_sorted = df.sort_values(by="Both Shafts total volume")
 df_sorted.to_csv('shaft_properties.csv', index=False)
